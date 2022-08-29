096b4b5c0be533a4f7d9b5572f4a529e8c55d177
Jovin <jovinpj@gmail.com>
2022-08-29 18:57:18 +0530

    NyNews: UnitTests added, dependencies required for UnitTests(Mockito, coroutines-test) also added

---
+++ NYNews/app/src/test/java/com/learn/nyNews/ui/base/BaseViewModelTest.kt
@@ -0,0 +1,38 @@
+package com.learn.nyNews.ui.base
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.mockito.MockitoAnnotations
+
+@ExperimentalCoroutinesApi
+@DelicateCoroutinesApi
+abstract class BaseViewModelTest {
+
+    private val mainThreadSurrogate = newSingleThreadContext("UI thread")
+    private lateinit var openMocks: AutoCloseable
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(mainThreadSurrogate)
+        openMocks = MockitoAnnotations.openMocks(this)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+        mainThreadSurrogate.close()
+        openMocks.close()
+    }
+
+}
